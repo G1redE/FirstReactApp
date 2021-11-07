1f491f3007f7d260d20228365d1a7c1a30347e08
G1redE <90474166+G1redE@users.noreply.github.com>
2021-11-07 19:58:47 +0530

    first commit

---
+++ src/components/firstassignment.js
@@ -0,0 +1,12 @@
+
+
+function FirstAssignment() {
+    return ( 
+        <div>
+            <h1>Hello World</h1>
+            <p>Welcome to my World</p>
+        </div>
+     );
+}
+
+export default FirstAssignment;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FirstAssignment from './components/firstassignment';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirstAssignment />
   </React.StrictMode>,
   document.getElementById('root')
 );
